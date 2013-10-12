ef6802ca4e75e3044102c713139e4c0cf1e228dd
matee <matee@matee.net>
2013-10-12 23:49:28 +0200

    judge http server

---
+++ src/judge/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"flag"
+	"log"
+	"time"
+	"strconv"
+	"fmt"
+)
+
+var port int
+
+func log_request(start time.Time, request *http.Request) {
+	log.Printf("\"%s %s\" %s \"%s\" %s",
+		request.Method,
+		request.URL.RequestURI(),
+		request.Proto,
+		request.UserAgent(),
+		time.Since(start),
+	)
+}
+
+func judgeHandler(res http.ResponseWriter, req *http.Request) {
+	defer log_request(time.Now(), req)
+	res.Header().Set("Content-Type", "text/plain")
+	res.Header().Set("Server", "judge/0.1")
+	req.ParseForm()
+	transactionId, _ := strconv.Atoi(req.Form.Get("transaction_id"))
+	if transactionId % 2 == 0 {
+		io.WriteString(res, "OK")
+	} else {
+		io.WriteString(res, "FAIL")
+	}
+}
+
+func parseArguments(){
+	flag.IntVar(&port, "port", 3000, "Port on which server will listen")
+	flag.Parse()
+}
+
+func main() {
+	parseArguments()
+	handlerFunc := http.HandlerFunc(judgeHandler)
+	addr := fmt.Sprintf(":%v", port)
+	log.Printf("Listening on port %v...", port)
+	log.Fatal(http.ListenAndServe(addr, handlerFunc))
+}
--- src/sync2async/main.go
+++ src/sync2async/main.go
@@ -18,7 +18,7 @@ type Request struct {
 func log_request(start time.Time, request *http.Request) {
 	log.Printf("\"%s %s\" %s \"%s\" %s",
 		request.Method,
-		request.URL.Path,
+		request.URL.RequestURI(),
 		request.Proto,
 		request.UserAgent(),
 		time.Since(start),
