3ee8b13cf393b215055422f16aed1670c3f6cefc
matee <matee@matee.net>
2013-10-13 12:54:27 +0200

    config file

--- src/sync2async/main.go
+++ src/sync2async/main.go
@@ -1,19 +1,27 @@
 package main
 
 import (
-//	"bufio"
+	"flag"
+	"fmt"
 	"io"
 	"log"
 	"net"
 	"net/http"
 	"os"
 	"time"
+	"encoding/json"
+	"io/ioutil"
 )
 
 type Request struct {
-	resultChan chan string
+	resultChan    chan string
 	transactionId string
 }
+
+type Config map[string]interface{}
+var (
+	config Config
+)
 
 func log_request(start time.Time, request *http.Request) {
 	log.Printf("\"%s %s\" %s \"%s\" %s",
@@ -25,20 +33,46 @@ func log_request(start time.Time, request *http.Request) {
 	)
 }
 
+func parseArguments() (config_path string) {
+	flag.StringVar(&config_path, "config", "sync2async.json", "Path to configuration file")
+	flag.Parse()
+	return
+}
+
+func loadConfig(config_path string, fail bool) {
+	file, err := ioutil.ReadFile(config_path)
+	if err != nil {
+		log.Println("open config: ", err)
+		if fail {
+			os.Exit(1)
+		}
+	}
+
+	var temp Config
+	if err = json.Unmarshal(file, &temp); err != nil {
+		log.Println("parse config: ", err)
+		if fail {
+			os.Exit(1)
+		}
+	}
+	config = temp
+}
+
+func init() {
+	config_path := parseArguments()
+	loadConfig(config_path, true)
+}
+
 func main() {
 	mapping := make(map[string]*Request)
-	connection, _ := net.Dial("tcp", "127.0.0.1:9001")
+	connection, _ := net.Dial("tcp", config["drm_addr"].(string))
 	defer connection.Close()
-
 
 	http_handler := func(res http.ResponseWriter, req *http.Request) {
 		defer log_request(time.Now(), req)
 		req.ParseForm()
 		transactionId := req.Form.Get("transaction_id")
 
-		// Ask judge...
-		// If OK, hit DRM server
-		// in other case, just return some error
 
 		request := Request{resultChan: make(chan string), transactionId: transactionId}
 		mapping[transactionId] = &request
@@ -51,16 +85,17 @@ func main() {
 		res.Header().Set("Content-Type", "text/plain")
 		res.Header().Set("Server", "nagra-proxy")
 		select {
-		case r := <- request.resultChan:
+		case r := <-request.resultChan:
 			io.WriteString(res, r)
-		case <-time.After(5 * time.Second):
+		case <-time.After(config["timeout"].(time.Duration) * time.Second):
 			io.WriteString(res, "zepsute")
 		}
-		
+
 	}
 
 	http.HandleFunc("/sync", http_handler)
-	log.Printf("Listening on 0.0.0.0:9000")
-	log.Fatal(http.ListenAndServe(":9000", nil))
+	addr := fmt.Sprintf(":%v", config["port"])
+	log.Printf("Listening on port %v", config["port"])
+	log.Fatal(http.ListenAndServe(addr, nil))
 	os.Exit(1)
 }
--- src/sync2async/main_test.go
+++ src/sync2async/main_test.go
@@ -1,7 +1,9 @@
 package main
+
 import "testing"
+
 // https://github.com/bmizerany/assert
 // http://nathany.com/go-testing-toolbox/
 func TestSth(t *testing.T) {
-	
+
 }
