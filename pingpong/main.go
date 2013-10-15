ea5584a45368ef97e6cbc8665e09a6cf9466e8e4
Łukasz Sągol <lukasz@sagol.pl>
2013-10-15 16:41:41 +0200

    pingpong server

---
+++ pingpong/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"net"
+	"log"
+	"io"
+	"bytes"
+	"math/rand"
+	"time"
+	"bufio"
+	"strings"
+)
+
+func handleRequest(connection net.Conn) {
+	// ignore input, just output
+	line, err := bufio.NewReader(connection).ReadBytes('\n')
+	if err != nil { log.Fatal(err) }
+
+	command := strings.ToUpper(string(line[:]))
+	log.Print(command)
+
+	sleep := rand.Intn(50) * 100
+	time.Sleep(time.Duration(sleep) * time.Millisecond)
+
+	io.Copy(connection, bytes.NewBufferString("PONG"))
+	connection.Close();
+}
+
+func main() {
+	sock, err := net.Listen("tcp", ":8000")
+	if err != nil { log.Fatal(err) }
+
+	for {
+		connection, err := sock.Accept()
+		if err != nil { log.Fatal(err) }
+
+		go handleRequest(connection)
+	}
+}
