157b8d26195202765b2613f0ef37f8c5ab2b3125
matee <matee@matee.net>
2013-10-17 21:59:29 +0200

    error response

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ import (
 	//"github.com/matee911/go-sync2async/judge"
 	"github.com/matee911/go-sync2async/logging"
 	//"github.com/matee911/go-sync2async/transaction"
+	"encoding/json"
+	"errors"
 	"io"
 	"log"
 	"net"
@@ -17,7 +19,6 @@ import (
 	"os"
 	"strconv"
 	"time"
-  "errors"
 )
 
 type Request struct {
@@ -63,35 +64,64 @@ func init() {
 }
 
 func validateAddress(s string) (int, error) {
-    if len(s) == 0 {
-        return 0, errors.New("address is empty")
-    } else if len(s) > 10 {
-        return 0, errors.New("address out of range")
-    } else if i, err := strconv.Atoi(s); err != nil {
-        return i, err
-    } else {
-      return i, nil
-    }
+	if len(s) == 0 {
+		return 0, errors.New("address is empty")
+	} else if len(s) > 10 {
+		return 0, errors.New("address out of range")
+	} else if i, err := strconv.Atoi(s); err != nil {
+		return i, err
+	} else {
+		return i, nil
+	}
 }
 
 func validateChipset(s string) (string, error) {
-  if len(s) == 18 {
-    return s, errors.New("invalid length of chipset_type_string")
-  } else {
-    return s, nil
-  }
+	if len(s) == 18 {
+		return s, errors.New("invalid length of chipset_type_string")
+	} else {
+		return s, nil
+	}
 }
 
 func validateContent(s string) (int, error) {
-  if len(s) == 0 {
-    return 0, errors.New("content is empty")
-  } else if len(s) > 9 {
-    return 0, errors.New("content out of range")
-  } else if i, err := strconv.Atoi(s); err != nil {
-    return i, err
-  } else {
-    return i, nil
-  }
+	if len(s) == 0 {
+		return 0, errors.New("content is empty")
+	} else if len(s) > 9 {
+		return 0, errors.New("content out of range")
+	} else if i, err := strconv.Atoi(s); err != nil {
+		return i, err
+	} else {
+		return i, nil
+	}
+}
+
+type ErrorResponse struct {
+	Resp ErrRespJSON `json:"resp"`
+}
+
+func (r ErrorResponse) String() (s string) {
+	body, err := json.Marshal(r)
+	if err != nil {
+		s = ""
+		return
+	}
+	s = string(body)
+	return
+}
+
+type ErrRespJSON struct {
+	Status string `json:"status"`
+	Ts     int    `json:"ts"`
+	//License *LicenseJSON `json:license`
+	ErrCode int    `json:"errcode"`
+	ErrDesc string `json:"errdesc"`
+	ErrText string `json:"err_text"`
+}
+
+type LicenseJSON struct {
+	Object           string `json:"object"`
+	ValidToTimestamp int    `json:"valid_to_timestamp"`
+	MetaData         string `json:"metadata"`
 }
 
 func licenseHttpHandler(mapping map[int]*Request) func(http.ResponseWriter, *http.Request) {
@@ -99,35 +129,35 @@ func licenseHttpHandler(mapping map[int]*Request) func(http.ResponseWriter, *htt
 		defer logging.HttpRequest(time.Now(), req)
 		req.ParseForm()
 
-    res.Header().Set("Content-Type", "application/json")
+		res.Header().Set("Content-Type", "application/json")
 		res.Header().Set("Server", userAgent)
 
 		// NUID (Nagra Unique IDentifier) [int, max 10-digits long]
 		address, err := validateAddress(req.PostForm.Get("address"))
-    if err!= nil {
-      http.Error(res, err.Error(), 400)
-      return
-    }
+		if err != nil {
+			http.Error(res, err.Error(), 400)
+			return
+		}
 
 		// Chipset Type String (please refer to DVS documentation)
 		// [text in format: xxxx xxxx xxxx xxxx xx]
 		chipset_type_string, err := validateChipset(req.PostForm.Get("chipset_type_string"))
-    if err!= nil {
-      http.Error(res, err.Error(), 400)
-      return
-    }
+		if err != nil {
+			http.Error(res, err.Error(), 400)
+			return
+		}
 
 		// Content identifier [int, max 9-digits long]
 		content_id, err := validateContent(req.PostForm.Get("content_id"))
-    if err != nil {
-      http.Error(res, err.Error(), 400)
-      return
-    }
+		if err != nil {
+			http.Error(res, err.Error(), 400)
+			return
+		}
 		// any binary string data, that will be sent to authorization module
 		// for additional authorization (eg. user credentials, token)
 		extra := req.PostForm.Get("extra")
 
-    log.Printf("addr: %d chip: %s content: %s extra: %s", address, chipset_type_string, content_id, extra)
+		log.Printf("addr: %d chip: %s content: %s extra: %s", address, chipset_type_string, content_id, extra)
 
 		//judge.AskForPermission("ala", &config)
 
@@ -177,16 +207,16 @@ func licenseHttpHandler(mapping map[int]*Request) func(http.ResponseWriter, *htt
 		     403
 		     500
 		*/
-		
-    /*
-		select {
-		case r := <-request.resultChan:
-			io.WriteString(res, r)
-		// TODO(m): ladowanie czasu z konfigu i castowanie
-		case <-time.After(5 * time.Second):
-			io.WriteString(res, "zepsute")
-		}
-    */
+
+		/*
+			select {
+			case r := <-request.resultChan:
+				io.WriteString(res, r)
+			// TODO(m): ladowanie czasu z konfigu i castowanie
+			case <-time.After(5 * time.Second):
+				io.WriteString(res, "zepsute")
+			}
+		*/
 	}
 }
 
@@ -198,7 +228,7 @@ func main() {
 	}
 	defer connection.Close()
 
-  // Heartbeat
+	// Heartbeat
 	heartbeat := time.NewTicker(time.Second * time.Duration(config.Heartbeat))
 	defer heartbeat.Stop()
 	go func() {
@@ -230,7 +260,7 @@ func main() {
 		//case <-time.After(time.Duration(config.Timeout) * time.Second):
 		//	io.WriteString(res, "zepsute")
 		//}
-    io.WriteString(res, "io")
+		io.WriteString(res, "io")
 	}
 
 	http.HandleFunc("/license", licenseHttpHandler(mapping))
--- main_test.go
+++ main_test.go
@@ -1,6 +1,20 @@
 package main
 
-//import "testing"
+import (
+	"github.com/matee911/go-sync2async/assert"
+	"testing"
+)
 
-// https://github.com/bmizerany/assert
-// http://nathany.com/go-testing-toolbox/
+func TestJSONErrorResponse(t *testing.T) {
+	assert.Equal(t, ErrorResponse{
+		Resp: ErrRespJSON{
+			Status:  "err",
+			Ts:      1234567890,
+			ErrCode: 2,
+			ErrDesc: "err desc",
+			ErrText: "err text"},
+	}.String(),
+		`{"resp":{"status":"err","ts":1234567890,"errcode":2,"errdesc":"err desc","err_text":"err text"}}`,
+	)
+
+}
